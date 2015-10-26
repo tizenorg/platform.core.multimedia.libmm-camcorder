fb65c872e72342ce7273d807666f1604c8a95950
Manish Sogi <manish.sogi@samsung.com>
2015-10-26 19:45:11 +0530

    Integration of libmm-camcorder with murphy resource manager
    Change-Id: Ic711b414012a7e2d8b0d12cd13de1ba3dc6ab8a4
    Signed-off-by: Manish Sogi <manish.sogi@samsung.com>

---
+++ src/include/mm_camcorder_resource.h
@@ -0,0 +1,55 @@
+/*
+ * libmm-camcorder
+ *
+ * Copyright (c) 2000 - 2011 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact: Heechul Jeon <heechul.jeon@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __MM_CAMCORDER_RESOURCE_H__
+#define __MM_CAMCORDER_RESOURCE_H__
+
+#include <murphy/plugins/resource-native/libmurphy-resource/resource-api.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+	RESOURCE_TYPE_CAMERA,
+	RESOURCE_TYPE_VIDEO_OVERLAY,
+} MMCamcorderResourceType;
+
+typedef struct {
+	mrp_mainloop_t *mloop;
+	mrp_res_context_t *context;
+	mrp_res_resource_set_t *rset;
+	bool is_connected;
+	void *user_data;
+} MMCamcorderResourceManager;
+
+int _mmcamcorder_resource_manager_init(MMCamcorderResourceManager *resource_manager, void *user_data);
+int _mmcamcorder_resource_manager_prepare(MMCamcorderResourceManager *resource_manager, MMCamcorderResourceType resource_type);
+int _mmcamcorder_resource_manager_acquire(MMCamcorderResourceManager *resource_manager);
+int _mmcamcorder_resource_manager_release(MMCamcorderResourceManager *resource_manager);
+int _mmcamcorder_resource_manager_unprepare(MMCamcorderResourceManager *resource_manager);
+int _mmcamcorder_resource_manager_deinit(MMCamcorderResourceManager *resource_manager);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MM_PLAYER_RESOURCE_H__ */
